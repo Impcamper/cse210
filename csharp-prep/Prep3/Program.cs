24074e31b97e1559e257f03a711014aab31c5589
Impcamper <impcamper@gmail.com>
2023-01-12 14:28:08 -0600

    guess number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        int guess = 0;
+        while (guess !=number){
+            Console.WriteLine("Guess a number");
+            guess = int.Parse(Console.ReadLine());
+            if(guess>number){
+                Console.WriteLine("Too high.");
+            }
+            else if(guess<number){
+                Console.WriteLine("Too low.");
+            }
+            else{
+                Console.WriteLine("Correct!");
+            }
+        }
     }
 }
