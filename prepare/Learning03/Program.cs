480a9d0abcd6c2d4a7bfd4749de29e5d38980c19
Impcamper <impcamper@gmail.com>
2023-01-31 13:50:00 -0600

    fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction test = new Fraction(4);
+        Console.WriteLine(test.GetFractionString());
+        Console.WriteLine(test.GetDecimalValue());
+        Console.WriteLine("Enter the two numbers");
+        int var1=int.Parse(Console.ReadLine());
+        int var2=int.Parse(Console.ReadLine());
+        Fraction test2 = new Fraction(var1,var2);
+        Console.WriteLine(test2.GetFractionString());
+        Console.WriteLine(test2.GetDecimalValue());
+    }
+}
+public class Fraction{
+    private int _top;
+    private int _bottom;
+    public Fraction(){
+        _top=1;
+        _bottom=1;
+    }
+    public Fraction(int wholeNumber){
+        _top=wholeNumber;
+        _bottom=1;
+    }
+    public Fraction(int top, int bottom){
+        _top=top;
+        _bottom=bottom;
+    }
+    public int GetTop(){
+        return _top;
+    }
+    public void SetTop(int top){
+        _top=top;
+    }
+    public int GetBottom(){
+        return _bottom;
+    }
+    public void SetBottom(int bottom){
+        _bottom=bottom;
+    }
+    public string GetFractionString(){
+        return _top+"/"+_bottom;
+    }
+    public double GetDecimalValue(){
+        return Convert.ToDouble(_top)/_bottom;
     }
 }
