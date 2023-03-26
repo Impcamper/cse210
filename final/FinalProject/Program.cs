edcbf888ba12e8264704e194523dcce630221ee5
Impcamper <impcamper@gmail.com>
2023-03-25 22:32:05 -0600

    techinly works?

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,55 +4,110 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        Console.WriteLine("testing that punching eachother works");
+        Player A = new Player(5,2);
+        Monster B = new Monster(8,1);
+        //figure out a better way of doing this, perhaps feeding the monster to player?
+        int choice=-1;
+        while(choice!=0){
+            Console.WriteLine("weee attackinggggg! enter 0 to end");
+            B.Damage(A.Attack());
+            A.Damage(B.Attackloop());
+            choice=int.Parse(Console.ReadLine());
+        }
     }
 }
 
 public class Player{
-    protected int hp;
-    int power;
-    public void Attack(){
-
- }
+    int _hp;
+    int _attack;
+    public Player(int hp, int attck){
+        _hp=hp;
+        _attack=attck;
+    }
+    public int Attack(){
+        //Make it random within 1 or two points of the value
+        return _attack;
+    }
+    public void Damage(int dmg){
+        _hp=-dmg;
+        if(_hp>=0){
+            Console.WriteLine("ded");
+        }
+    }
 }
 public class Melee:Player{
+    public Melee(int hp, int attck):base(hp,attck){
+
+    }
     public void Defend(){
 
     }
 }
 public class Ranged:Player{
+    public Ranged(int hp, int attck):base(hp,attck){
+
+    }
     public void Retreat(){
         
     }
 }
 public class Skill:Player{
+    public Skill(int hp, int attck):base(hp,attck){
+
+    }
     public void Recover(){
         
     }
 }
 public class Monster{
-    int _counter;
-    int _hp;
-    int _attack;
-    public Monster(){
+    protected int _counter;
+    protected int _hp;
+    protected int _attack;
+    public Monster(int hp, int attack){
         _counter=0;
+        _hp=hp;
+        _attack=attack;
     }
-    public virtual void Attackloop(){
-
+    public virtual int Attackloop(){
+        if (_counter==0){
+            _counter++;
+            //Make it random within 1 or two points of the value
+            return _attack;
+        }
+        else{
+            _counter=0;
+            return 0;
+        }
+    }
+    public void Damage(int dmg){
+        _hp=-dmg;
+        if(_hp>=0){
+            Console.WriteLine("ded");
+        }
     }
 }
 public class TypeA: Monster{
-    public override void Attackloop(){
+    public TypeA(int hp, int attack): base(hp,attack){
 
+    }
+    public override int Attackloop(){
+        return _attack;
     }
 }
 public class TypeB: Monster{
-    public override void Attackloop(){
+    public TypeB(int hp, int attack): base(hp,attack){
 
+    }
+    public override int Attackloop(){
+        return _attack;
     }
 }
 public class TypeC: Monster{
-    public override void Attackloop(){
+    public TypeC(int hp, int attack): base(hp,attack){
 
+    }
+    public override int Attackloop(){
+        return _attack;
     }
 }
