7aa5962926e374b04578043742dfb1dd7167707c
Impcamper <impcamper@gmail.com>
2023-04-01 18:02:39 -0600

    Funny code bit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Thing test = new Thing();
+        var hello = test.Write;
+        hello();
+    }
+}
+public class Thing{
+    public void Write(){
+        Console.WriteLine("Test sucesse");
     }
 }
